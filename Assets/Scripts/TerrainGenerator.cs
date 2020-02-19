a33bb2e9fa56b4e2e0098227d6ee7ba8b34eb56b
Korey Kinney <kkinney2@student.fitchburgstate.edu>
2020-02-19 13:25:11 -0500

    TerrainGenerator
    Bool to toggle and generate objs in edit mode

--- Assets/Scripts/TerrainGenerator.cs
+++ Assets/Scripts/TerrainGenerator.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class TerrainGenerator : MonoBehaviour
 {
     private GameManager gameManager;
@@ -10,6 +11,10 @@ public class TerrainGenerator : MonoBehaviour
 
     public ObjectToSpawn[] objsToSpawn;
 
+    [Space(10)]
+
+    public bool generateTerrain = false;
+
     private void Awake()
     {
         gameManager = GameObject.Find("@GameManager").GetComponent<GameManager>();
@@ -17,29 +22,44 @@ public class TerrainGenerator : MonoBehaviour
 
     private void Start()
     {
-        StartCoroutine(Setup());
+        //StartCoroutine(Setup());
     }
 
-    IEnumerator Setup()
+    private void Update()
     {
-        yield return new WaitForEndOfFrame();
+        if (generateTerrain)
+        {
+            //StartCoroutine(Setup());
+            Setup();
+            generateTerrain = false;
+        }
+    }
 
+    private void Setup()
+    {
         Vector2 halfBoundingObjScale = new Vector2(Ground.transform.localScale.x / 2f, Ground.transform.localScale.z / 2f);
 
         foreach (ObjectToSpawn objectToSpawn in objsToSpawn)
         {
-            GameObject folder = new GameObject();
-            folder.name = objectToSpawn.obj.name + "s";
+            if (objectToSpawn.folder != null)
+            {
+                objectToSpawn.folder.name += "_DELETE";
+                objectToSpawn.folder.SetActive(false);
+            }
+
+
+            objectToSpawn.folder = new GameObject();
+            objectToSpawn.folder.name = objectToSpawn.obj.name + "s";
 
             for (int i = 0; i < objectToSpawn.numToSpawn; i++)
             {
                 Vector3 spawnPos = new Vector3
                 (
-                    Ground.transform.position.x + Random.Range(-halfBoundingObjScale.x, halfBoundingObjScale.x),
+                    Ground.transform.position.x + Random.Range(-halfBoundingObjScale.x+1, halfBoundingObjScale.x-1),
                     0,
-                    Ground.transform.position.y + Random.Range(-halfBoundingObjScale.y, halfBoundingObjScale.y)
+                    Ground.transform.position.y + Random.Range(-halfBoundingObjScale.y+1, halfBoundingObjScale.y-1)
                 );
-                GameObject temp = Instantiate(objectToSpawn.obj, folder.transform);
+                GameObject temp = Instantiate(objectToSpawn.obj, objectToSpawn.folder.transform);
                 temp.transform.position = spawnPos;
             }
         }
@@ -51,4 +71,5 @@ public class ObjectToSpawn
 {
     public int numToSpawn;
     public GameObject obj;
+    public GameObject folder;
 }
